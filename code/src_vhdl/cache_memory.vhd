eecba16b8c0d661cbd7cfbf0ee3fb3f57cd1201b
Jeremy Michaud <jeremy.michaud@heig-vd.ch>
2016-05-27 09:53:25 +0200

    Cache basique comme en page 14

--- code/src_vhdl/cache_memory.vhd
+++ code/src_vhdl/cache_memory.vhd
@@ -57,11 +57,27 @@ end cache_memory;
 
 
 architecture struct of cache_memory is
-    constant NUMBER_OF_WORDS_IN_CACHE_LINE : integer := 16 -- Nombre de mots par ligne de cache
-    type cache_type is array (0 to (2**INDEX_SIZE)-1) of std_logic_vector(NUMBER_OF_WORDS_IN_CACHE_LINE * DATA_SIZE);
+    constant NUMBER_OF_WORDS_IN_CACHE_LINE : integer := 16; -- Nombre de mots par ligne de cache
+    constant OFFSET_SIZE : integer := 4; -- LOG2(NUMBER_OF_WORDS_IN_CACHE_LINE)
+    type cache_type is array (0 to (2**INDEX_SIZE)-1) of std_logic_vector((NUMBER_OF_WORDS_IN_CACHE_LINE * DATA_SIZE)-1 downto 0);
+    type bit_array_type is array (0 to (2**INDEX_SIZE)-1) of std_logic;
+    type tag_type   is array (0 to (2**INDEX_SIZE)-1) of std_logic_vector(TAG_SIZE-1 downto 0);
     
-    signal cache : cache_type;
+    signal cache : cache_type;            -- La cache (lignes de mots)
+    signal dirty_bits : bit_array_type;   -- Les bits dirty de la cache
+    signal valid_bits : bit_array_type;   -- Les bits valid
+    signal tags : tag_type;               -- La liste des tags
+
+    -- Signaux d'entrée de la cache
+    signal cache_index_s : std_logic_vector(INDEX_SIZE-1 downto 0);
+    signal cache_tag_s : std_logic_vector(TAG_SIZE-1 downto 0);
+    signal cache_b_off_s : std_logic_vector(OFFSET_SIZE-1 downto 0);
+    signal cache_read_s : std_logic;
+    signal cache_write_s : std_logic;
     
+    -- Signaux de sortie de la cache
+    signal cache_data_s : std_logic_vector(DATA_SIZE-1 downto 0);
+    signal cache_hit_s : std_logic;
 begin
     -- These asserts are used by simulation in order to check the generic 
     -- parameters with the instanciation of record ports
@@ -76,8 +92,12 @@ begin
     Cache_process : process (clk_i, reset_i) is
     begin
       if (reset_i = '1') then
+        valid_bits <= (others => '0'); -- Au reset toutes les lignes de cache
+                                       -- sont invalidées
       elsif rising_edge(clk_i) then
-        
+        cache_hit_s <= '1' when ((tags(to_integer(unsigned(cache_index_s))) = cache_tag_s) and valid_bits(to_integer(unsigned(cache_index_s))) = '1')
+                       '0';
+        cache_data_s <= cache(((to_integer(unsigned(cache_b_off_s))+1) * DATA_SIZE) - 1 downto to_integer(unsigned(cache_b_off_s)) * DATA_SIZE);
       end if;
     end process;
 
